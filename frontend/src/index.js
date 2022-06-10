47c98554234f0befe6d11841dd406789424f9ce3
Aleksandr <kechko02@gmail.com>
2022-06-10 22:37:01 +0300

    initial commit for front

---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'core-js/stable';
+
+// import './models/init';
+import Main from './components/Main';
+// import './fonts/fonts.css';
+
+ReactDOM.render(<Main />, document.getElementById('root'));
