b42e12f3ebe5074f3925e16496bbcc50529267cc
bzelijah <ilya.andreev@onlinesup.com>
2022-06-11 18:08:35 +0300

    tasks and users interfaces

--- sql/init.sql
+++ sql/init.sql
@@ -1,9 +1,29 @@
 CREATE TABLE tasks (
     id                  SERIAL,
-    external_id         text         DEFAULT '',
-    created_at          timestamp    DEFAULT CURRENT_TIMESTAMP,
-    disabled_at         timestamp,
-    name                text         NOT NULL,
-    priority            integer      NOT NULL,
-    disabled            boolean      DEFAULT false
+    created_at          timestamp       DEFAULT CURRENT_TIMESTAMP,
+    companyName         text            DEFAULT '',
+    sphere              text            DEFAULT '',
+    vacancyName         text            DEFAULT '',
+    name                text            DEFAULT '',
+    conditions          text            DEFAULT '',
+    tasks               text ARRAY      DEFAULT ARRAY[''],
+    info                text ARRAY      DEFAULT ARRAY[''],
+    salary              text            DEFAULT '',
+    skills              text ARRAY      DEFAULT ARRAY[''],
+    email               text            DEFAULT '',
+    age                 int             DEFAULT 0,
+    date                timestamp
 );
+
+CREATE TABLE users (
+    id                  SERIAL,
+    name                text            DEFAULT '',
+    secondName          text            DEFAULT '',
+    birthdayDate        text            DEFAULT '',
+    gender              text            DEFAULT '',
+    phone               text            DEFAULT '',
+    email               text            DEFAULT '',
+    password            text            DEFAULT '',
+    work                text            DEFAULT '',
+    englishLevel        text            DEFAULT ''
+)
