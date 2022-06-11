2aa08b64794e43ddefab89db09969e090f12609b
Aleksandr <kechko02@gmail.com>
2022-06-12 00:36:36 +0300

    refactoring homepage

---
+++ frontend/src/components/HomePage/Hands/styles.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+
+import { Button } from '../../../components/common/Button/styles';
+
+export const Hands = styled.div`
+	max-width: 1440px;
+  height: 780px;
+
+  margin-right: auto;
+  margin-left: auto;
+
+  background-position: center center;
+  background-size: cover;
+
+	padding: 100px 0 76px;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+
+	.title {
+		font-size: 48px;
+		line-height: 62px;
+		font-family: 'Rubik Medium';
+		text-align: center;
+		color: ${({ theme }) => theme.white};
+	}
+
+	.buttons-container {
+		display: flex;
+		justify-content: center;
+
+		button:first-child {
+			margin-right: 24px;
+		}
+	}
+`;
+
+export const StyledButton = styled(Button)`
+	color: ${({ theme }) => theme.white};
+	background-color: ${({ theme }) => theme.green};
+	border: ${({ theme }) => `6px solid ${theme.green}`};
+
+	padding: 30px 35px;
+	font-size: 25px;
+	line-height: 45px;
+
+	width: 320px;
+
+	${({ dark, theme }) => dark && `
+		background-color: ${theme.purple};
+		border 6px solid ${theme.purple};
+	`}
+`;
+
+
+
+
--- frontend/src/components/HomePage/SearchBar/index.js
+++ frontend/src/components/HomePage/SearchBar/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { useHistory } from 'react-router'
+import { useHistory } from 'react-router';
 
 
 export const SearchBar = () => {
-    const goToSearch = useHistory()
-    return (
-        <div onClick={() => goToSearch.push('/search')}>Поиск того рот</div>
-    );
+	const goToSearch = useHistory();
+	return (
+		<div onClick={() => goToSearch.push('/search')}>Поиск того рот</div>
+	);
 };
