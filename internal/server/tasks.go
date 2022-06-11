cf0532ca151ae9a873639e479c903bf2bc63dac3
bzelijah <ilya.andreev@onlinesup.com>
2022-06-11 22:22:19 +0300

    decomposition

--- internal/server/tasks.go
+++ internal/server/tasks.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"errors"
 	"github.com/Bzelijah/case-2/configs"
 	"github.com/Bzelijah/case-2/internal/logger"
 	"github.com/labstack/echo/v4"
@@ -10,17 +11,26 @@ import (
 func (s *server) tasks(c echo.Context) error {
 	filter := c.QueryParam("filter")
 	if filter == "false" || filter == "" {
-		tasks, err := s.pgDto.GetTasks()
+		tasks, err := s.getTasksWithoutFilter()
 		if err != nil {
 			logger.Error(CTX).Err(err).Msg("error while get all tasks")
 			return c.NoContent(fasthttp.StatusConflict)
 		}
 
-		if len(tasks) < 1 {
-			logger.Error(CTX).Msg("tasks not found")
-		}
 		return c.Blob(fasthttp.StatusOK, "application/json", configs.JSONMarshal(tasks))
 	}
 
 	return c.NoContent(fasthttp.StatusOK)
+}
+
+func (s *server) getTasksWithoutFilter() ([]configs.Task, error) {
+	tasks, err := s.pgDto.GetTasks()
+	if err != nil {
+		return []configs.Task{}, err
+	}
+	if len(tasks) < 1 {
+		return []configs.Task{}, errors.New("tasks not found")
+	}
+
+	return tasks, nil
 }
