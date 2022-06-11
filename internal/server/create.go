77146006339302a375a4e074379ac926783ea5c4
bzelijah <ilya.andreev@onlinesup.com>
2022-06-11 20:29:15 +0300

    create route with insert task to db

--- internal/server/create.go
+++ internal/server/create.go
@@ -2,18 +2,25 @@ package server
 
 import (
 	"encoding/json"
-	"fmt"
+	"github.com/Bzelijah/case-2/configs"
+	"github.com/Bzelijah/case-2/internal/logger"
 	"github.com/labstack/echo/v4"
 	"github.com/valyala/fasthttp"
 )
 
 func (s *server) create(c echo.Context) error {
-	var task task
+	var task configs.Task
 	err := json.NewDecoder(c.Request().Body).Decode(&task)
 	if err != nil {
+		logger.Error(CTX).Err(err).Msg("decode body")
 		return c.String(fasthttp.StatusBadRequest, err.Error())
 	}
-	fmt.Println(task)
 
-	return c.NoContent(fasthttp.StatusNotFound)
+	err = s.pgDto.CreateTask(task)
+	if err != nil {
+		logger.Error(CTX).Err(err).Interface("task", task).Msg("error while create task")
+		return c.NoContent(fasthttp.StatusConflict)
+	}
+
+	return c.NoContent(fasthttp.StatusOK)
 }
--- internal/server/interfaces.go
+++ internal/server/interfaces.go
@@ -9,16 +9,3 @@ type server struct {
 	echoServer *echo.Echo
 	pgDto      *pg.Dto
 }
-
-type task struct {
-	CompanyName string   `json:"companyName" db:"company_name"`
-	Sphere      string   `json:"sphere" db:"sphere"`
-	VacancyName string   `json:"vacancyName" db:"vacancy_name"`
-	Tasks       []string `json:"tasks" db:"tasks"`
-	Skills      []string `json:"skills" db:"skills"`
-	Conditions  []string `json:"conditions" db:"conditions"`
-	Motivation  []string `json:"motivation" db:"motivation"`
-	Email       []string `json:"email" db:"email"`
-	MinAge      int      `json:"minAge" db:"min_age"`
-	MaxAge      int      `json:"maxAge" db:"max_age"`
-}
