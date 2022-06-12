b75b4e8d8be0f171fdabcc4d8d27d8537e423028
Aleksandr <kechko02@gmail.com>
2022-06-13 02:26:21 +0300

    add style for create task

--- frontend/src/pages/CreateTask/index.js
+++ frontend/src/pages/CreateTask/index.js
@@ -2,142 +2,148 @@ import React from 'react';
 
 import * as S from './styles';
 
+import attach from '../../assets/attach.svg';
+
 export const CreateTaskPage = () => {
 	return (
 		<S.CreateTaskPage>
-			<h1>Анкета для оформления волонтерской деятельности</h1>
+			<S.Title>Анкета для оформления волонтерской деятельности</S.Title>
 			<form>
 				<S.Step>
-					<h4>ШАГ 1</h4>
-					<h4>ПЕРСОНАЛЬНЫЕ ДАННЫЕ</h4>
-					<p>Заполняются автоматически из Личного кабинета. Если хотите изменить данные, отредактируйте профиль.</p>
-				</S.Step>
-				<S.Label>
-					Заявитель
-					<input placeholder={'Иван Иванович Иванов'} type={'text'} name={'name'}/>
+					<div className='step'>ШАГ 1</div>
+					<div className='step-title'>ПЕРСОНАЛЬНЫЕ ДАННЫЕ</div>
+					<div className='summary'>Заполняются автоматически из Личного кабинета. Если хотите изменить данные, отредактируйте профиль.</div>
+					<S.Label>
+						Заявитель
+					</S.Label>
+					<S.Input placeholder={'Иван Иванович Иванов'} type={'text'} name={'name'} />
 					<S.Checkbox>
-						<input type={'checkbox'} name={'individual'} id={'individual'} value={'individual'}/>
+						<input type={'radio'} name={'face'} id={'individual'} value={'individual'} />
 						<label htmlFor={'individual'}>Физическое лицо</label>
 					</S.Checkbox>
 					<S.Checkbox>
-						<input type={'checkbox'} name={'entity'} id={'entity'} value={'entity'}/>
+						<input type={'radio'} name={'face'} id={'entity'} value={'entity'} />
 						<label htmlFor={'entity'}>Юридическое лицо</label>
 					</S.Checkbox>
-					<input placeholder={'Наименование организации заявителя'} type={'text'} name={'companyName'}/>
-				</S.Label>
-				<S.Label>
-					Контактные данные
-					<input placeholder={'example@example.com'} name={'email'} id={'email'}/>
-				</S.Label>
-				<S.Step>
-					<h4>ШАГ 2</h4>
-					<h4>ОФОРМЛЕНИЕ ЗАЯВКИ</h4>
+					<S.Input noLabel={true} placeholder={'Наименование организации заявителя'} type={'text'} name={'companyName'} />
+					<S.Label>
+						Контактные данные
+					</S.Label>
+					<S.Input placeholder={'example@example.com'} name={'email'} id={'email'} />
 				</S.Step>
-				<S.Label>
-					Вакансия
-					<input placeholder={'Опишите тему мероприятия и причину его проведения'} name={'vacancyName'} id={'vacancyName'}/>
-				</S.Label>
-				<S.Label>
-					Формат мероприятия
+				<S.Step>
+					<div className='step'>ШАГ 2</div>
+					<div className='step-title'>ОФОРМЛЕНИЕ ЗАЯВКИ</div>
+					<S.Label>
+						Вакансия
+					</S.Label>
+					<S.Textarea placeholder={'Опишите тему мероприятия и причину его проведения'} name={'vacancyName'} id={'vacancyName'} />
+					<S.Label>
+						Формат мероприятия
+					</S.Label>
 					<S.Checkbox>
-						<input type={'checkbox'} name={'offline'} id={'offline'} value={'offline'}/>
+						<input type={'radio'} name={'online'} id={'offline'} value={'offline'} />
 						<label htmlFor={'offline'}>Оффлайн</label>
 					</S.Checkbox>
 					<S.Checkbox>
-						<input type={'checkbox'} name={'online'} id={'online'} value={'online'}/>
+						<input type={'radio'} name={'online'} id={'online'} value={'online'} />
 						<label htmlFor={'online'}>Онлайн</label>
 					</S.Checkbox>
-				</S.Label>
-				<S.Label>
-					Направление
+					<S.Label>
+						Направление
+					</S.Label>
 					<S.Checkbox>
-						<input type={'checkbox'} name={'sport'} id={'sport'} value={'sport'}/>
+						<input type={'radio'} name={'type'} id={'sport'} value={'sport'} />
 						<label htmlFor={'sport'}>Спортивная</label>
 					</S.Checkbox>
 					<S.Checkbox>
-						<input type={'checkbox'} name={'social'} id={'social'} value={'social'}/>
+						<input type={'radio'} name={'type'} id={'social'} value={'social'} />
 						<label htmlFor={'social'}>Социальная</label>
 					</S.Checkbox>
 					<S.Checkbox>
-						<input type={'checkbox'} name={'culture'} id={'culture'} value={'culture'}/>
+						<input type={'radio'} name={'type'} id={'culture'} value={'culture'} />
 						<label htmlFor={'culture'}>Культурная</label>
 					</S.Checkbox>
 					<S.Checkbox>
-						<input type={'checkbox'} name={'sphere-different'} id={'sphere-different'} value={'sphere-different'}/>
+						<input type={'radio'} name={'type'} id={'sphere-different'} value={'sphere-different'} />
 						<label htmlFor={'sphere-different'}>Другое</label>
 					</S.Checkbox>
-					<input placeholder={'Тип направления  волонтерской деятельности'} type={'text'} name={'sphere'}/>
-				</S.Label>
-				<S.Label>
-					Необходимые навыки
+					<S.Textarea placeholder={'Тип направления  волонтерской деятельности'} type={'text'} name={'sphere'} />
+					<S.Label>
+						Необходимые навыки
+					</S.Label>
 					<S.Checkbox>
-						<input type={'checkbox'} name={'volunteer'} id={'volunteer'} value={'volunteer'}/>
+						<input type={'radio'} name={'role'} id={'volunteer'} value={'volunteer'} />
 						<label htmlFor={'volunteer'}>Волонтёр</label>
 					</S.Checkbox>
 					<S.Checkbox>
-						<input type={'checkbox'} name={'organizer'} id={'organizer'} value={'organizer'}/>
+						<input type={'radio'} name={'role'} id={'organizer'} value={'organizer'} />
 						<label htmlFor={'organizer'}>Организатор</label>
 					</S.Checkbox>
 					<S.Checkbox>
-						<input type={'checkbox'} name={'manager'} id={'manager'} value={'manager'}/>
+						<input type={'radio'} name={'role'} id={'manager'} value={'manager'} />
 						<label htmlFor={'manager'}>Менеджер</label>
 					</S.Checkbox>
 					<S.Checkbox>
-						<input type={'checkbox'} name={'skills-different'} id={'skills-different'} value={'skills-different'}/>
+						<input type={'radio'} name={'role'} id={'skills-different'} value={'skills-different'} />
 						<label htmlFor={'skills-different'}>Другое</label>
 					</S.Checkbox>
-				</S.Label>
-				<S.Label>
-					Сервисы для волонтёров
-					<input placeholder={'Напишите какие сервисы (удобства) будут предоставлены волонтерам'} type={'text'} name={'conditions'}/>
-				</S.Label>
-				<S.Label>
-					Мотивации для волонтёров
-					<input placeholder={'Напишите какие сервисы (удобства) будут предоставлены волонтерам'} type={'text'} name={'motivation'}/>
-				</S.Label>
-				<S.Label>
-					Возраст участников
+					<S.Textarea placeholder={'Какие навыки необходимы для участия в мероприятии'} type={'text'} name={'sphere'} />
+					<S.Label>
+						Сервисы для волонтёров
+					</S.Label>
+					<S.Textarea placeholder={'Напишите какие сервисы (удобства) будут предоставлены волонтерам'} type={'text'} name={'conditions'} />
+					<S.Label>
+						Мотивации для волонтёров
+					</S.Label>
+					<S.Textarea placeholder={'Напишите какие сервисы (удобства) будут предоставлены волонтерам'} type={'text'} name={'motivation'} />
+					<S.Label>
+						Возраст участников
+					</S.Label>
 					<S.Checkbox>
-						<input type={'checkbox'} name={'age-0'} id={'age-0'} value={'18-25'}/>
+						<input type={'radio'} name={'age'} id={'age-0'} value={'18-25'} />
 						<label htmlFor={'age-0'}>18-25</label>
 					</S.Checkbox>
 					<S.Checkbox>
-						<input type={'checkbox'} name={'age-1'} id={'age-1'} value={'26-35'}/>
+						<input type={'radio'} name={'age'} id={'age-1'} value={'26-35'} />
 						<label htmlFor={'age-1'}>26-35</label>
 					</S.Checkbox>
 					<S.Checkbox>
-						<input type={'checkbox'} name={'age-2'} id={'age-2'} value={'35-55'}/>
+						<input type={'radio'} name={'age'} id={'age-2'} value={'35-55'} />
 						<label htmlFor={'age-2'}>35-55</label>
 					</S.Checkbox>
 					<S.Checkbox>
-						<input type={'checkbox'} name={'age-3'} id={'age-3'} value={'55+'}/>
+						<input type={'radio'} name={'age'} id={'age-3'} value={'55+'} />
 						<label htmlFor={'age-3'}>55+</label>
 					</S.Checkbox>
-				</S.Label>
-				<S.Label>
-					Даты проведения
+					<S.Label>
+						Даты проведения
+					</S.Label>
 					<S.Checkbox>
-						<input type={'checkbox'} name={'date-0'} id={'date-0'} value={'Срочно'}/>
+						<input type={'radio'} name={'date'} id={'date-0'} value={'Срочно'} />
 						<label htmlFor={'date-0'}>Срочно</label>
 					</S.Checkbox>
 					<S.Checkbox>
-						<input type={'checkbox'} name={'date-1'} id={'date-1'} value={'Текущий месяц'}/>
+						<input type={'radio'} name={'date'} id={'date-1'} value={'Текущий месяц'} />
 						<label htmlFor={'date-1'}>Текущий месяц</label>
 					</S.Checkbox>
 					<S.Checkbox>
-						<input type={'checkbox'} name={'date-2'} id={'date-2'} value={'В ближайшее время'}/>
+						<input type={'radio'} name={'date'} id={'date-2'} value={'В ближайшее время'} />
 						<label htmlFor={'date-2'}>В ближайшее время</label>
 					</S.Checkbox>
-					<input placeholder={'ДД.ММ.ГГГГ'} type={'text'} name={'date-3'} id={'date-3'}/>
-				</S.Label>
-				<S.Label>
-					Материал для публикации
-					<input placeholder={'Здесь вы можете написать сопроводительное письмо к приложенным материалам'} type={'text'} name={'date-3'} id={'date-3'}/>
-					<button>Добавить файл</button>
-					<p>Вы можете добавить до 10 файлов PNG, PDF, JPEG, JPG, BMP, DOC, DOCX, RTF, XLS, XLSX, TXT, размер одного — до 5 МБ.</p>
-				</S.Label>
-				<button>Отправить заявку</button>
+					<S.Input noLabel={true} date={true} placeholder={'ДД.ММ.ГГГГ'} type={'text'} name={'date-3'} id={'date-3'} />
+					<S.Label>
+						Материал для публикации
+					</S.Label>
+					<S.Textarea placeholder={'Здесь вы можете написать сопроводительное письмо к приложенным материалам'} type={'text'} name={'date-3'} id={'date-3'} />
+					<S.DownloadButton>
+						<img src={attach} />
+						Добавить файл
+					</S.DownloadButton>
+					<S.Summary>Вы можете добавить до 10 файлов PNG, PDF, JPEG, JPG, BMP, DOC, DOCX, RTF, XLS, XLSX, TXT, размер одного — до 5 МБ.</S.Summary>
+				</S.Step>
+				<S.ConfirmButton>Отправить заявку</S.ConfirmButton>
 			</form>
-		</S.CreateTaskPage>
+		</S.CreateTaskPage >
 	);
 };
